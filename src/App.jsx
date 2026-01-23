15abdd4ac3aa4e69ab45d90de6c4f06ee18782da
mad <144912170+rafiahmadr@users.noreply.github.com>
2026-01-23 22:41:46 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ScrollToTop from "./components/ScrollToTop"; 
 import Home from "./pages/Home";
 import About from "./pages/About";
 import ProjectDetail from "./pages/ProjectDetail";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion as Motion } from "framer-motion";
 
 export default function App() {
+  const location = useLocation();
+
   return (
     <>
-      {/* AnimatePresence only affects page transitions */}
+      <ScrollToTop />
+      <Navbar />
+
       <AnimatePresence mode="wait">
-        <ScrollToTop />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects/:id" element={<ProjectDetail />} />
-        </Routes>
+        <Motion.div
+          key={location.pathname}
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.4 }}
+        >
+          <Routes location={location} key={location.pathname}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects/:id" element={<ProjectDetail />} />
+          </Routes>
+        </Motion.div>
       </AnimatePresence>
     </>
   );
