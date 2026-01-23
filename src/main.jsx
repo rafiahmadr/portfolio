6cccadf2b4c0ef559df5617e90043b6df3f50a67
mad <144912170+rafiahmadr@users.noreply.github.com>
2026-01-23 22:48:04 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-
+import { HashRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
 
 // Optional: update document title
 document.title = "Rafi Ahmad R | Portfolio";
