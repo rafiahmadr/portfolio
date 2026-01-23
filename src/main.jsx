11a01309a88beecec9b8b08dd1a4b8a66da4326f
mad <144912170+rafiahmadr@users.noreply.github.com>
2026-01-23 22:34:29 +0700

    Update document title and clean up render
    Removed title element from render and updated document title.

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,13 @@ import App from "./App";
 import "./index.css";
 import { HashRouter } from "react-router-dom";
 
+// Optional: update document title
+document.title = "Rafi Ahmad R | Portfolio";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
-    <App />
-    <title>Rafi Ahmad R | Portfolio</title>
+      <App />
     </HashRouter>
   </React.StrictMode>
 );
-
-const redirect = sessionStorage.redirect;
-if (redirect) {
-  sessionStorage.removeItem("redirect");
-  window.history.replaceState(null, "", redirect);
-}
