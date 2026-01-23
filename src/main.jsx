2236bf5f433d21e1b9ee75ddacab11e6d439d23f
mad <rafiahmadr19@gmail.com>
2026-01-23 21:54:47 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <HashRouter>
     <App />
     <title>Rafi Ahmad R | Portfolio</title>
+    </HashRouter>
   </React.StrictMode>
 );
